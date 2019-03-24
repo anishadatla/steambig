321701c7f10bbcf27667b95038b415da9d9b5683
Anisha Datla <anidatla@amazon.com>
2019-03-23 23:30:02 -0400

    list

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-
+function genList(){
+  var data1 = JSON.parse(data);
+  var index = 0;
+  while(index != 2){
+      var list = document.getElementsByClassName("example")[0];
+      list.getElementsByClassName("child")[index].innerHTML = data1[index].name + "\n " + data1[index].GhgeKg + "\n " + data1[index].GhgeKcal;
+      index++;
+    }
+}
 
 function checkValues(obj, key){
 
