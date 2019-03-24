4dcf0eed53bd40fec2c903b5057d9f9390ce15b0
Anisha Datla <anidatla@amazon.com>
2019-03-23 23:47:52 -0400

    delete MAMP

--- sample.py
+++ sample.py
@@ -10,19 +10,19 @@ with open(fname,'r') as fr:
 for i in range(1,len(data)):
 
     if data[i][0] not in proddict:
-        proddict[data[i][0].lower()] = {}
-        proddict[data[i][0].lower()]["GHGEs per 100g median"] = data[i][2]
-        proddict[data[i][0].lower()]["GHGEs per 100 kcal median"] = data[i][3]
+        proddict[data[i][0].lower()]["name"] = data[i][0] 
+        proddict[data[i][0].lower()]["GhgeKg"] = data[i][2]
+        proddict[data[i][0].lower()]["GhgeKcal"] = data[i][3]
 
 scrapedProducts = []
 scrapedProducts.append("beef")
 scrapedProducts.append("cheese")
 
-finaldict = {}
+finaldict = []
 for prod in scrapedProducts:
     for k,v in proddict.items():
-        if prod in k:
-            finaldict[k] = v
+        finaldict.append(v);
+
 
 with open("data.json", 'w') as fw:
     fw.write(json.dumps(finaldict))
